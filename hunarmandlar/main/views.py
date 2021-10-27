242ff8dc085637ea3100274c2c0a3fb7636c22ff
Abdulhamid <85868288+Axmadandi@users.noreply.github.com>
2021-10-27 16:09:50 +0300

    awsefcdewa

--- hunarmandlar/main/views.py
+++ hunarmandlar/main/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View,ListView
+from .models import *
 
 # Create your views here.
 class HomeView(ListView):
     def get(self,request):
-        return render(request,'index.html')
+        cats = Category.objects.all()
+        context = {
+            'cats':cats
+        }
+        return render(request,'index.html',context)
+
+def cat_detail(request, slug):
+    cat = Category.objects.get(slug=slug)
+    context = {
+        'cat':cat,
+    }
+    return render(request, 'index2.html', context)
